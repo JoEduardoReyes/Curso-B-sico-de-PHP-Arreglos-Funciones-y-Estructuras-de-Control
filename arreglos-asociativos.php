383c31a05247bcd7ef732959b1fc0965169c6bdd
Eduardo Reyes <joeduardoreyes@outlook.com>
2022-08-29 17:32:35 -0600

    Arreglos asociativos: experimento

--- arreglos-asociativos.php
+++ arreglos-asociativos.php
@@ -14,15 +14,24 @@
 
 // echo "El precio del cafe americano es {$cafes["American"]} dolares";
 
+$colegios = array(
+  "Guillermo Putzeys", "La patria", "Sor Juana"
+);
+
 $personas = array(
   "Carlos" => array(
     "Edad" => 20,
     "Apellido" => "Santana",
+    "Colegio" => $colegios[0]
   ),
     "Michi" => array(
     "Edad" => 18,
     "Apellido" => "Michisancio",
+    "Colegio" => $colegios[1]
   ),
 );
 
-echo "La informacion de Michi es: Edad: " . $personas['Michi']['Edad'] . " Apellido: " . $personas['Michi']['Apellido'];
+
+echo "La informacion de Michi es: Edad: " . $personas['Michi']['Edad'] . " Apellido: " . $personas['Michi']['Apellido'] . " y estudio en " . $personas['Michi']['Colegio'] . "\n";
+
+var_dump($personas['Michi']['Colegio'] === $colegios[1]);
