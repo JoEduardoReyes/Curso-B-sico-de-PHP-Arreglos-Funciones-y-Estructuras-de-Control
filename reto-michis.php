df333db862f8ef9a921df02ac0e509ec1da1366e
Eduardo Reyes <joeduardoreyes@outlook.com>
2022-09-01 17:08:24 -0600

    reto de los michis

--- reto-michis.php
+++ reto-michis.php
@@ -1,8 +1,22 @@
 <?php
 
 $michis_list = array(
-  "Gatuno" => array(
+  "Michiduardo" => array(
     "color" => "gris",
-    ""
+    "ocupacion" => "desarrollador",
+    "comida" => array(
+      "favorita" => "pescado",
+      "rechazada" => "brocoli",
+    )
+  ),
+    "Michiberto" => array(
+    "color" => "blanco",
+    "ocupacion" => "maestro",
+    "comida" => array(
+      "favorita" => "res",
+      "rechazada" => "papas",
+    )
   )
 );
+
+var_dump($michis_list["Michiduardo"]["comida"]["favorita"]);
