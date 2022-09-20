1f24b5090bfb62b79add66e376316eba8502e0cc
Eduardo Reyes <joeduardoreyes@outlook.com>
2022-09-19 18:17:50 -0600

    condicionales

---
+++ if-else.php
@@ -0,0 +1,12 @@
+<?php
+
+$asiento_disponibles = 0;
+$es_hijo_de_Tom_holland = true;
+
+if ($asiento_disponibles > 0) {
+  echo "Disponibles: ".$asiento_disponibles;
+} else if ($es_hijo_de_Tom_holland) {
+  echo "No te creo pero puedes ver la pelicula";
+} else {
+  echo "No hay asientos disponibles.";
+}
