b16a6401d859952079954be5e3c99066a0191ad1
Eduardo Reyes <joeduardoreyes@outlook.com>
2022-09-27 10:46:26 -0600

    Reto de caminos posibles a la tienda terminado

--- reto-tiendas.php
+++ reto-tiendas.php
@@ -2,7 +2,6 @@
 
 $contador1 = 0;
 $contador2 = 1;
-$contador3;
 $caminos = array(0, 0);
 
 $tienda = readline('A que tienda quieres llegar? ');
@@ -15,12 +14,13 @@ if ($tienda == 1)
 
 if ($tienda > 1) {
   for ($i=0; $i < $tienda; $i++) {
-  $contador1 = $contador1 + $contador2;
-  $contador2 = $contador1 + $contador2;
-  $contador3 = $contador1 + $contador2;
-  array_push($caminos, $contador1, $contador2);
+    $contador1 = $contador1 + $contador2;
+    $contador2 = $contador1 + $contador2;
+    if (count($caminos) <= $tienda) {
+    array_push($caminos, $contador1, $contador2);
+    } else {
+      break;
+    }
   }
+  echo "Existen $caminos[$tienda] caminos para ir a la tienda $tienda \n";
 };
-
-
-echo "Existen $caminos[$tienda] caminos para ir a la tienda $tienda";
