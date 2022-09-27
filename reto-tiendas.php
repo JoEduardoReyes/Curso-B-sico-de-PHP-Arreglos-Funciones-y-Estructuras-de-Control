10a23b39dbe06b48f49cc6d2e8c1f3cc5f10dcd9
Eduardo Reyes <joeduardoreyes@outlook.com>
2022-09-27 10:20:56 -0600

    Reto de caminos posibles hasta la tienda: mi solucion

--- reto-tiendas.php
+++ reto-tiendas.php
@@ -3,15 +3,24 @@
 $contador1 = 0;
 $contador2 = 1;
 $contador3;
-$caminos = array(0, 0, 1);
+$caminos = array(0, 0);
 
 $tienda = readline('A que tienda quieres llegar? ');
 
-for ($i=0; $i < $tienda; $i++) {
+if ($tienda <= 0)
+  echo "No elegiste la tienda correctamente";
+
+if ($tienda == 1)
+  echo "Ya estás en la primera tienda";
+
+if ($tienda > 1) {
+  for ($i=0; $i < $tienda; $i++) {
   $contador1 = $contador1 + $contador2;
   $contador2 = $contador1 + $contador2;
   $contador3 = $contador1 + $contador2;
-  array_push($caminos, $contador2, $contador3);
-}
+  array_push($caminos, $contador1, $contador2);
+  }
+};
 
-var_dump($caminos);
+
+echo "Existen $caminos[$tienda] caminos para ir a la tienda $tienda";
